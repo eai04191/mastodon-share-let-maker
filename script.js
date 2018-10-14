bd6699c00359a7f69dec882dc4726162eaf4ee04
eai04191 <eai@mizle.net>
2018-10-14 21:47:18 +0900

    Change to hide old results when there is no input text.

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let input = document.getElementById("instance");
+const input = document.getElementById("instance");
 
-let makeLet = function() {
-  let value = input.value;
-  console.log(value);
-  if (value) {
-    let instance = getInstance(value);
-    let result = document.getElementById("result");
-    result.innerHTML = `<h2><a href="javascript:void(open('https://${instance}/share?text='+encodeURIComponent(document.title)+'%20'+encodeURIComponent(document.location)))">Toot! at ${instance}</a></h2>`;
-    let usage = document.getElementById("usage");
-    usage.style = "display: block;";
+const makeLet = function() {
+  const result = document.getElementById("result");
+  const usage = document.getElementById("usage");
+  if (input.value.length == 0) {
+    result.style = "display: none;";
+    usage.style = "display: none;";
+  } else {
+    const value = input.value;
+    if (value) {
+      const instance = getInstance(value);
+      result.innerHTML = `<h2><a href="javascript:void(open('https://${instance}/share?text='+encodeURIComponent(document.title)+'%20'+encodeURIComponent(document.location)))">Toot! at ${instance}</a></h2>`;
+      result.style = "display: block;";
+      usage.style = "display: block;";
+    }
   }
 };
 
